4dff559691910d585ce542d0b1f573f6111c8275
Hadi493 <hadialam493@gmail.com>
2025-10-22 19:32:56 +0600

    ...

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -1,8 +1,14 @@
+-- ==============================
+--  LSP + CMP Configuration
+-- ==============================
 
-local cmp = require('cmp')
-local luasnip = require('luasnip')
+-- Safe require
+local cmp = require("cmp")
+local luasnip = require("luasnip")
 
--- Setup nvim-cmp
+-- ==============================
+--  nvim-cmp Setup
+-- ==============================
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -10,114 +16,143 @@ cmp.setup({
     end,
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-b>"]   = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"]   = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"]   = cmp.mapping.abort(),
+    ["<CR>"]    = cmp.mapping.confirm({ select = true }),
   }),
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
   }, {
-    { name = 'buffer' },
-    { name = 'path' },
+    { name = "buffer" },
+    { name = "path" },
   }),
 })
 
--- Setup lspconfig
+-- ==============================
+--  Keymaps on LSP Attach
+-- ==============================
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(args)
+    local bufnr = args.buf
 
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-    callback = function(args)
-        local bufnr = args.buf
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
+    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-        -- Enable completion triggered by <c-x><c-o>
-        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-        -- Mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local bufopts = { noremap=true, silent=true, buffer=bufnr }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-        vim.keymap.set('n', '<space>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, bufopts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-        vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-        vim.keymap.set('n', '<space>gr', vim.lsp.buf.references, bufopts)
-        vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, bufopts)
-        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, bufopts)
-        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, bufopts)
-        vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, bufopts)
-    end,
+    local opts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K",  vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set("n", "<space>wl", function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<space>gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
+  end,
 })
 
+-- ==============================
+--  LSP Servers
+-- ==============================
 
--- Configure specific language servers
-vim.lsp.config('lua_ls', {
+-- Lua
+vim.lsp.config("lua_ls", {
   settings = {
     Lua = {
-      runtime = {
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        globals = {'vim'},
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      telemetry = {
-        enable = false,
+      runtime = { version = "LuaJIT" },
+      diagnostics = { globals = { "vim" } },
+      workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+      telemetry = { enable = false },
+    },
+  },
+})
+vim.lsp.enable("lua_ls")
+
+-- Python
+vim.lsp.config("pyright", {})
+vim.lsp.enable("pyright")
+
+-- Rust
+vim.lsp.config("rust_analyzer", {})
+vim.lsp.enable("rust_analyzer")
+
+-- TypeScript/JavaScript
+vim.lsp.config("ts_ls", {})
+vim.lsp.enable("ts_ls")
+
+-- Bash
+vim.lsp.config("bashls", {})
+vim.lsp.enable("bashls")
+
+-- Docker
+vim.lsp.config("dockerls", {})
+vim.lsp.enable("dockerls")
+
+-- JSON
+vim.lsp.config("jsonls", {})
+vim.lsp.enable("jsonls")
+
+-- YAML
+vim.lsp.config("yamlls", {})
+vim.lsp.enable("yamlls")
+
+-- Markdown
+vim.lsp.config("marksman", {})
+vim.lsp.enable("marksman")
+
+-- XML
+vim.lsp.config("lemminx", {})
+vim.lsp.enable("lemminx")
+
+-- ==============================
+--  Grammar (LTeX)
+-- ==============================
+vim.lsp.config("ltex", {
+  settings = {
+    ltex = {
+      language = "en-US",
+      disabledRules = {
+        ["en-US"] = { "MORFOLOGIK_RULE_EN_US" },
       },
     },
   },
 })
-vim.lsp.enable('lua_ls')
+vim.lsp.enable("ltex")
 
-vim.lsp.config('pyright', {})
-vim.lsp.enable('pyright')
-
-vim.lsp.config('rust_analyzer', {})
-vim.lsp.enable('rust_analyzer')
-
-vim.lsp.config('ts_ls', {})
-vim.lsp.enable('ts_ls')
-
-vim.lsp.config('bashls', {})
-vim.lsp.enable('bashls')
-
-vim.lsp.config('dockerls', {})
-vim.lsp.enable('dockerls')
-
-vim.lsp.config('jsonls', {})
-vim.lsp.enable('jsonls')
-
-vim.lsp.config('marksman', {})
-vim.lsp.enable('marksman')
-
-vim.lsp.config('lemminx', {})
-vim.lsp.enable('lemminx')
-
-vim.lsp.config('yamlls', {})
-vim.lsp.enable('yamlls')
-
--- Setup ltex for grammar correction
-vim.lsp.config('ltex', {
-    settings = {
-        ltex = {
-            language = "en-US",
-            disabledRules = {
-                ["en-US"] = { "MORFOLOGIK_RULE_EN_US" }
-            }
-        }
-    }
+-- ==============================
+--  ✅ NEW: C/C++ (clangd)
+-- ==============================
+vim.lsp.config("clangd", {
+  cmd = { "clangd", "--background-index" },
 })
-vim.lsp.enable('ltex')
+vim.lsp.enable("clangd")
+
+-- ==============================
+--  ✅ NEW: Assembly / FASM (asm-lsp)
+-- ==============================
+vim.lsp.config("asm_lsp", {
+  cmd = { "asm-lsp" },
+  filetypes = { "asm", "s", "S" },
+})
+vim.lsp.enable("asm_lsp")
+
+-- Optional: Toast on LSP attach
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    vim.notify("LSP Attached: " .. client.name)
+  end,
+})
+
